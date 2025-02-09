8dcfae686708b325752fc2681e3861f4e79654c8
@suraj05 <wadikarsuraj02@gmail.com>
2025-02-09 19:04:39 +0530

    ADDED : Splash Screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -1,14 +1,37 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {screenHeight, screenWidth} from '../utils/Constants';
+import googleMeetImage from './../assets/images/g.png';
+import {resetAndNavigate} from '../utils/NavigationUtils';
 
 const Splash = () => {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      resetAndNavigate('home');
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={googleMeetImage} />
     </View>
   );
 };
 
 export default Splash;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: screenWidth * 0.7,
+    height: screenHeight * 0.7,
+    resizeMode: 'contain',
+  },
+});
